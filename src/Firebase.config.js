4fa923bde23e4befcaf27c81ce8898873c621fdf
Shakib448 <shakiba448@gmail.com>
2020-09-29 06:52:00 +0600

    Name added

--- src/Firebase.config.js
+++ src/Firebase.config.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBTFjnzU9C1gHP4atgRAe0dSkuimP7XjPE",
-  authDomain: "react-bruj-al-arab.firebaseapp.com",
-  databaseURL: "https://react-bruj-al-arab.firebaseio.com",
-  projectId: "react-bruj-al-arab",
-  storageBucket: "react-bruj-al-arab.appspot.com",
-  messagingSenderId: "52302892040",
-  appId: "1:52302892040:web:5344422bf559d337fc15bd",
-  measurementId: "G-41MLXMF3VS",
+  apiKey: "AIzaSyCuW-iPY9HVk-xBrT9sL2TI7tgrpq5-leQ",
+  authDomain: "fire-auth-9ce8d.firebaseapp.com",
+  databaseURL: "https://fire-auth-9ce8d.firebaseio.com",
+  projectId: "fire-auth-9ce8d",
+  storageBucket: "fire-auth-9ce8d.appspot.com",
+  messagingSenderId: "1014930320940",
+  appId: "1:1014930320940:web:5cefcef13c9b2c2bb96b89",
 };
 
 export default firebaseConfig;
--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { UserContext } from "../../App";
 
 const Book = () => {
-    const {bedType} = useParams();
-    return (
-        <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
-            <p>Want a <Link to="/home">different room?</Link> </p>
-        </div>
-    );
+  const { bedType } = useParams();
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>
+        {" "}
+        Hello {loggedInUser.name} Let's book a {bedType} Room.
+      </h1>
+      <p>
+        Want a <Link to="/home">different room?</Link>{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Book;
